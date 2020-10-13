4e2e019213dc591da2e0668422726637bec98e73
Jcoderre <joeysteam@outlook.com>
2020-10-13 15:28:31 -0400

    Add Gem Devise and Rails_Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get 'user/index'
+  get 'user/show'
+  get 'user/new'
+  get 'user/edit'
+  get 'user/create'
+  get 'user/destroy'
+  get 'user/update'
+  devise_for :users
+  
   get 'residential/index'
   get 'application/index'
   root 'pages#index'
