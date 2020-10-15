a6caf802730960cb9c80e24294781db1aaf92fd9
Cindy Okino <cindy.okino@gmail.com>
2020-10-15 17:40:58 -0400

    create _quote-form.html.erb inside partials

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,4 +1,21 @@
 class FormController < ApplicationController
   def index
   end
+
+  def create
+    # @quotes = Quote.new(my_params)
+
+    puts "FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    
+    puts params[:NbApparts]
+    
+    # qu = Quote.new
+    # qu.totalCost = 15
+    # qu.InstallationFee = 30 
+    # qu.TotalNBofElevator = 80
+    # qu.save
+    # puts @quotes.NbApparts
+
+    # @quotes.save
+  end
 end
