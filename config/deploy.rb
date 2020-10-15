d662f61176d462bf7b37fa226f2814763b63aa05
MathieuHoude <mathieu.houde@codeboxx.biz>
2020-10-15 15:58:51 -0400

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ namespace :deploy do
  task :yarn_install do 
    on roles(:web) do 
      within release_path do 
-       execute("cd #{release_path} && yarn install") 
+       execute("cd #{release_path} && yarn install --check-files") 
      end 
    end 
  end 
