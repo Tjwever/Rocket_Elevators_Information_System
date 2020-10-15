f9126bf44e26fc4a9a67cfd7ae944e96c97a7c7a
Cindy Okino <cindy.okino@gmail.com>
2020-10-15 19:59:24 -0400

    Modify form_controller.rb

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -3,13 +3,27 @@ class FormController < ApplicationController
   end
 
   def create
-    puts "FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"    
+
+
     puts 
     
     quote = Quote.new
     quote.NbApparts = params[:NbApparts]
     quote.NbFloors = params[:NbFloors]   
     quote.NbBasements = params[:NbBasements]   
+    quote.NbBusiness = params[:NbBusiness]   
+    quote.NbParking = params[:NbParking]   
+    quote.NbCages = params[:NbCages]   
+    quote.NbDistinctTenant = params[:NbDistinctTenant]   
+    quote.nbOccup = params[:nbOccup]   
+    quote.Activity24 = params[:Activity24]   
+    quote.ElevatorChoice = params[:ElevatorChoice] 
+    quote.NbElevator = params[:NbElevator]   
+    quote.UnitPrice = params[:UnitPrice]   
+    quote.PriceElevator = params[:PriceElevator] 
+    quote.InstallationFee = params[:InstallationFee]   
+    quote.Total = params[:Total]    
+
 
     # quote.save
   end
