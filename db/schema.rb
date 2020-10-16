314a7bd6bb4e168b1de2d84658369c6057d37e93
Jcoderre <joeysteam@outlook.com>
2020-10-15 21:47:26 -0400

    admin_panel access to master only

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_175935) do
+ActiveRecord::Schema.define(version: 2020_10_16_001352) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "FirstName"
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(version: 2020_10_14_175935) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "superadmin_role", default: false
+    t.boolean "employee_role", default: false
+    t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
