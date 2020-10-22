aa95e32e23e86e058bb434447075f9d640556dd7
saadeddinne <saadeddine.feki@gmail.com>
2020-10-22 15:42:54 -0400

    shcema

--- db/schema.rb
+++ db/schema.rb
@@ -6,9 +6,7 @@
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+
 
 ActiveRecord::Schema.define(version: 2020_10_22_183602) do
 
