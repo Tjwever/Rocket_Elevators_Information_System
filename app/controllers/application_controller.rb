b7ddaf913370ed836ac5ef23c6ef969a8f31fb49
saadeddinne <saadeddine.feki@gmail.com>
2020-10-23 13:49:33 -0400

    before deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
+require 'rake'
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     skip_before_action :verify_authenticity_token
 
     def index
     end
+   
 end
