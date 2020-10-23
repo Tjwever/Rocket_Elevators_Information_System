e32ee5a4db98121285e82dedc6f7c8aa62f86c5e
Cindy Okino <cindy.okino@gmail.com>
2020-10-23 13:56:24 -0400

    bd warehouse chargé apres apppuier le boutton charts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
+require 'rake'
+
+Rake::Task.clear
+RocketElevatorsInformationSystem::Application.load_tasks
+
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     skip_before_action :verify_authenticity_token
 
+    def postgres_update
+        Rake::Task["dwhmanager:transfert"].reenable
+        Rake::Task["dwhmanager:transfert"].execute
+        redirect_to blazer_path
+      end 
+
     def index
     end
 end
+
